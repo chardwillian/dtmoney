18e7095bdffbd9b7f26990739e016ba3ee753527
chardwillian <69554100+chardwillian@users.noreply.github.com>
2021-12-01 15:07:08 -0300

    creating dashboard and modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
+import { NewTransactionModal } from './components/NewTransactionModal/index';
+
+Modal.setAppElement('#root');
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+  
   return (
-    <div className="App">
-     <h1>Hello World</h1>
+    <>
+     <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+     <Dashboard />
+     <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal}/>
      <GlobalStyle />
-    </div>
+    </> 
   );
 }
