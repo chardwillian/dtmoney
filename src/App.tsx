ec10aed1f313940f9520b2c977ab4023a445b51d
chardwillian <69554100+chardwillian@users.noreply.github.com>
2021-12-04 11:05:09 -0300

    finished

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import { useState } from 'react';
 import Modal from 'react-modal';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
-import { GlobalStyle } from "./styles/global";
 import { NewTransactionModal } from './components/NewTransactionModal/index';
+import { TransactionsProvider } from './hooks/useTransactions';
+
+import { GlobalStyle } from "./styles/global";
 
 Modal.setAppElement('#root');
 
@@ -19,11 +21,18 @@ export function App() {
   }
   
   return (
-    <>
+    <TransactionsProvider>
      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+
      <Dashboard />
-     <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal}/>
+
+     <NewTransactionModal 
+     isOpen={isNewTransactionModalOpen} 
+     onRequestClose={handleCloseNewTransactionModal}
+     />
+
      <GlobalStyle />
-    </> 
+     
+    </TransactionsProvider> 
   );
 }
